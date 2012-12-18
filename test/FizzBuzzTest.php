26d4e6f0bc19b16c37966f3556ea13a01fea2306
Eiji Ienaga <eiji.ienaga@gmail.com>
2012-12-18 11:26:18 +0900

    体裁を整えた

--- test/FizzBuzzTest.php
+++ test/FizzBuzzTest.php
@@ -1,39 +1,32 @@
 <?php
-class FizzBuzz {
-
-  public function converts($numbers)
+class FizzBuzzTest extends PHPUnit_Framework_TestCase {
+  public function testFIZZBUZZ配列を返すこと()
   {
+    $target = new FizzBuzz();
+    $expected = array(1, 2, "FIZZ", 4 , "BUZZ", "FIZZ", 7, 8, "FIZZ", "BUZZ", 11, "FIZZ", 13, 14, "FIZZBUZZ");
+    $this->assertEquals($expected, $target->converts(range(1, 15)));
+  }
+}
+
+class FizzBuzz {
+  public function converts($numbers) {
     $self = $this;
     return __::map($numbers, function($n) use($self) {
       return $self->convert($n);
     });
   }
 
-  public function convert($num)
-  {
+  public function convert($num) {
     if ($num % 15 == 0) {
       return "FIZZBUZZ";
     }
-
     if ($num % 3 == 0) {
       return "FIZZ";
     }
-
     if ($num % 5 == 0) {
       return "BUZZ";
     }
     return $num;
-  }
-}
-
-class FizzBuzzTest extends PHPUnit_Framework_TestCase
-{
-
-  public function testFIZZBUZZ配列を返すこと()
-  {
-    $target = new FizzBuzz();
-    $expected = array(1, 2, "FIZZ", 4 , "BUZZ", "FIZZ", 7, 8, "FIZZ", "BUZZ", 11, "FIZZ", 13, 14, "FIZZBUZZ");
-    $this->assertEquals($expected, $target->converts(range(1, 15)));
   }
 }
 ?>
